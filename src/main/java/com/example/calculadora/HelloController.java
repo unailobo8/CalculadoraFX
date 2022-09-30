8b512f82e7fe0bc8739b5cdb1c57f3f01359c40f
unknown <2dam3@HZ114484.H012108.NET>
2022-09-30 13:26:48 +0200

    añadir bottones controller

--- src/main/java/com/example/calculadora/HelloController.java
+++ src/main/java/com/example/calculadora/HelloController.java
@@ -1,14 +1,71 @@
 package com.example.calculadora;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Button button0;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+    @FXML
+    private Button buttonC;
+    @FXML
+    private Button buttonDel;
+    @FXML
+    private Button buttonDiv;
+    @FXML
+    private Button buttonMult;
+    @FXML
+    private Button buttonRes;
+    @FXML
+    private Button buttonSum;
+    @FXML
+    private Button buttonAns;
+    @FXML
+    private Button buttonComa;
+    @FXML
+    private Button buttonTrans;
+    @FXML
+    private TextField fieldNumbers;
+
+    public void cleanScreen(){
+
     }
+
+    public void makeOperation(){
+
+    }
+
+    public void delValue(){
+
+    }
+
+    public void addValues(ActionEvent event){
+
+    }
+
+    public void addOperation(ActionEvent event){
+
+    }
+
 }
